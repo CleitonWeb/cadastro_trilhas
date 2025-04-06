378ba0910a9f13738d4348a271c9a93b81b2ac5c
Cleiton Pereira <cleiton.sp06@gmail.com>
2025-04-05 21:30:10 -0300

    Subindo projeto

---
+++ js/app.js
@@ -0,0 +1,128 @@
+const arrDados = document.querySelectorAll('.dados');
+
+
+function submeteForm(){
+  if(validarCampo()) certificaDados();
+}
+
+
+
+function validarCampo() {
+  const msgErro = `Campo de preenchimento obrigatório!`;
+  let retorno;
+
+    for (let i = 0; i < arrDados.length; i++){
+      let idCampo = arrDados[i].id;
+      let valorCampo = arrDados[i].value.trim();
+      let spanErro  = document.getElementById(`error_${idCampo}`);
+
+        if(valorCampo === ''){
+          spanErro.innerHTML = msgErro;
+          window.scrollTo(0, 0);
+          retorno = false;
+        }else{
+          if(idCampo === 'email'){
+            retorno = validarEmail(valorCampo, idCampo);
+          }else if(idCampo === 'cpf'){
+            retorno = validarCPF();
+          }else if(idCampo === 'telefone'){
+            retorno = validarTelefone();
+          }else if(idCampo === 'cep'){
+            retorno = validarCEP();
+          }else{
+            spanErro.innerHTML = '';
+            retorno = true;
+          }
+        }
+    }
+    
+    return retorno;
+  }
+
+
+  const campos = document.querySelectorAll('input[type="text"]');
+
+  campos.forEach(campo => {
+    campo.addEventListener('keydown', () => {      
+      document.getElementById(`error_${campo.id}`).innerHTML = '';
+    });
+  });
+
+// Obtém os elementos
+const modal = document.getElementById("modal");
+const botaoFechar = document.querySelector(".fechar");
+const botaoCancelar = document.getElementById('btnCancelar');
+
+
+// Abre a tela suspensa com os dados preenchidos (div modal)
+function certificaDados() {
+  modal.style.display = "block";
+
+  let stringHtml = '';
+  
+  for (let i = 0; i < arrDados.length; i++){
+    let idCampo = arrDados[i].id;
+    let valorCampo = arrDados[i].value;
+    let nomeCampo = idCampo.charAt(0).toUpperCase() + idCampo.slice(1); 
+    
+    if (idCampo === 'dtNasc'){
+      const data = new Date(valorCampo);
+      valorCampo = data.toLocaleDateString();      
+      stringHtml += `<span>Data de Nascimento: </span><strong><span>${valorCampo}</span></strong></p>`;   
+    }else if(idCampo === 'numero'){
+      stringHtml += `<span>Número: </span><strong><span>${valorCampo}</span></strong></p>`;
+    }else{
+      stringHtml += `<span>${nomeCampo}: </span><strong><span>${valorCampo}</span></strong></p>`;    
+    }    
+  }
+  
+  document.getElementById("dados_preenchidos").innerHTML = stringHtml;
+  window.scrollTo(0, 0);
+}
+
+
+// Fecha o modal quando o botão de fechar é clicado
+botaoFechar.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Fecha o modal quando o botão de fechar é clicado
+botaoCancelar.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+
+function carregaDados(){  
+  localStorage.clear();
+
+  for (let i = 0; i < arrDados.length; i++){
+    let idCampo = arrDados[i].id;
+    let valorCampo = arrDados[i].value;
+
+    if(valorCampo){
+      localStorage.setItem(idCampo, valorCampo);
+    }    
+  }
+
+  if(localStorage.length > 0){
+    alert('Dados salvos com sucesso!');
+    modal.style.display = "none";
+    modal_login.style.display = "block";
+    
+    document.getElementById('login').value = localStorage.getItem('cpf');
+  }  
+}
+
+
+
+const trilhas = document.querySelectorAll('.trilha-opcao');
+
+trilhas.forEach(trilha => {
+  trilha.addEventListener('click', () => {
+    // Remove a classe 'selecionada' de todas as trilhas
+    trilhas.forEach(t => t.classList.remove('selecionada'));
+    // Adiciona a classe 'selecionada' à trilha clicada
+    trilha.classList.add('selecionada');
+  });
+});
---
+++ js/tools.js
@@ -0,0 +1,160 @@
+
+
+const estados = [
+    { uf: 'AC', nome: 'Acre' },
+    { uf: 'AL', nome: 'Alagoas' },
+    { uf: 'AP', nome: 'Amapá' },
+    { uf: 'AM', nome: 'Amazonas' },
+    { uf: 'BA', nome: 'Bahia' },
+    { uf: 'CE', nome: 'Ceará' },
+    { uf: 'DF', nome: 'Distrito Federal' },
+    { uf: 'ES', nome: 'Espírito Santo' },
+    { uf: 'GO', nome: 'Goiás' },
+    { uf: 'MA', nome: 'Maranhão' },
+    { uf: 'MT', nome: 'Mato Grosso' },
+    { uf: 'MS', nome: 'Mato Grosso do Sul' },
+    { uf: 'MG', nome: 'Minas Gerais' },
+    { uf: 'PA', nome: 'Pará' },
+    { uf: 'PB', nome: 'Paraíba' },
+    { uf: 'PR', nome: 'Paraná' },
+    { uf: 'PE', nome: 'Pernambuco' },
+    { uf: 'PI', nome: 'Piauí' },
+    { uf: 'RJ', nome: 'Rio de Janeiro' },
+    { uf: 'RN', nome: 'Rio Grande do Norte' },
+    { uf: 'RS', nome: 'Rio Grande do Sul' },
+    { uf: 'RO', nome: 'Rondônia' },
+    { uf: 'RR', nome: 'Roraima' },
+    { uf: 'SC', nome: 'Santa Catarina' },
+    { uf: 'SP', nome: 'São Paulo' },
+    { uf: 'SE', nome: 'Sergipe' },
+    { uf: 'TO', nome: 'Tocantins' }
+  ];
+  
+  // Exemplo de como gerar o elemento <select> usando JavaScript puro:
+  function gerarSelectEstados(idSelect) {
+    const select = document.getElementById(idSelect);
+  
+    estados.forEach(estado => {
+      const option = document.createElement('option');
+      option.value = estado.uf;
+      option.textContent = estado.nome;
+      select.appendChild(option);
+    });
+  }
+  
+  // Chame a função passando o ID do elemento <select>
+  gerarSelectEstados('estado');
+
+
+  function formatarCPF(campo) {
+    let cpf = campo.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
+    cpf = cpf.replace(/(\d{3})(\d{2})$/, '$1-$2');
+    campo.value = cpf;
+  }
+
+
+
+  function validarCPF() {
+    const cpf = document.getElementById('cpf').value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    const mensagemErro = document.getElementById(`error_cpf`);
+
+    if (cpf.length !== 11) {
+      mensagemErro.textContent = 'CPF inválido.';
+      return false;
+    }
+
+    if (/^(\d)\1+$/.test(cpf)) {
+      mensagemErro.textContent = 'CPF inválido.';
+      return false;
+    }
+
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+      soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let resto = 11 - (soma % 11);
+    const digito1 = resto >= 10 ? 0 : resto;
+
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+      soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = 11 - (soma % 11);
+    const digito2 = resto >= 10 ? 0 : resto;
+
+    if (parseInt(cpf.charAt(9)) === digito1 && parseInt(cpf.charAt(10)) === digito2) {
+      mensagemErro.textContent = ''; // Limpa a mensagem de erro
+      return true; // CPF válido
+    } else {
+      mensagemErro.textContent = 'CPF inválido.';
+      return false; // CPF inválido
+    }
+  }
+
+  
+
+function validarEmail(email, idCampo) {
+  const mensagemErro = document.getElementById(`error_${idCampo}`);
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (regexEmail.test(email)) {
+    mensagemErro.textContent = '';
+    return true;  
+  } else {
+    mensagemErro.textContent = 'E-mail inválido!';
+    return false;
+  }
+}
+
+
+function formatarTelefone(campo) {
+  let telefone = campo.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  telefone = telefone.replace(/^(\d{2})(\d)/g, '($1)$2'); // Coloca parênteses em volta dos dois primeiros dígitos
+  telefone = telefone.replace(/(\d{5})(\d)/, '$1-$2'); // Coloca hífen entre o quarto e quinto dígitos
+  campo.value = telefone;
+}
+
+
+function validarTelefone() {
+  const telefone = document.getElementById('telefone').value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  const mensagemErro = document.getElementById('error_telefone');
+
+  if (telefone.length !== 11) {
+    mensagemErro.textContent = 'Telefone inválido.';
+    return false;
+  }else{
+    mensagemErro.textContent = ''; // Limpa a mensagem de erro
+    return true; // Telefone válido
+  }
+
+
+}
+
+function formatarNumero(campo) {
+  let numero = campo.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  campo.value = numero;
+}
+
+function formatarCEP(campo) {
+  let cep = campo.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  cep = cep.replace(/(\d{5})(\d{3})/, '$1-$2'); // Adiciona o hífen
+  campo.value = cep;
+}
+
+
+
+function validarCEP() {
+  const cep = document.getElementById('cep').value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  const mensagemErro = document.getElementById('error_cep');
+
+  if (cep.length !== 8) {
+    mensagemErro.textContent = 'CEP inválido.';
+    return false;
+  }else{
+    mensagemErro.textContent = ''; // Limpa a mensagem de erro
+    return true; // CEP válido
+  }
+
+}
